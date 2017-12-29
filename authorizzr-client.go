eba21a3c096d7facbd0fd927aaa687ae7a812fd8
matjazonline <matjazonline@gmail.com>
2017-12-29 12:10:57 +0100

    extract token aud and user id

--- authorizzr-client.go
+++ authorizzr-client.go
@@ -9,6 +9,7 @@ import (
 	"context"
 	"strings"
 	"io/ioutil"
+	"github.com/dgrijalva/jwt-go"
 )
 
 func ValidateToken(tokenCtxKey interface{}, authorizzrCheckTokenUrl string, apiKey string) adaptr.Adapter {
@@ -17,6 +18,7 @@ func ValidateToken(tokenCtxKey interface{}, authorizzrCheckTokenUrl string, apiK
 			tknCtxVal := r.Context().Value(tokenCtxKey)
 			if tknCtxVal != nil && isTokenValid(r.Context(), tknCtxVal.(string), authorizzrCheckTokenUrl, apiKey) == nil {
 				h.ServeHTTP(w, r)
+				return
 			}
 			http.Error(w, "Not authorized", http.StatusForbidden)
 		})
@@ -36,18 +38,91 @@ func isTokenValid(ctx context.Context, token string, authorizzrCheckTokenUrl str
 	if err != nil {
 		return err
 	}
-
 	client := urlfetch.Client(ctx)
 	response, err := client.Post(authorizzrCheckTokenUrl, "application/json", strings.NewReader(string(bodyJson)))
 	if err != nil {
+		//fmt.Println("AUTH ERR=",err.Error())
 		return err
 	}
 	defer response.Body.Close()
 
 	responseJson, err := ioutil.ReadAll(response.Body)
-	if err != nil || string(responseJson) != `{"valid":true}` {
+
+	//fmt.Println("CHECKING AUTH resp=", string(responseJson),"err=",err, "apiKey=", apiKey, " token=",token)
+
+	if err == nil && string(responseJson) != `{"valid":true}` {
+		err=errors.New(string(responseJson))
+	}
+	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func UserIdentAndAudience2Ctx(ctxTokenKey interface{}, ctxUserIdentKey interface{}, tknAudKey ) adaptr.Adapter {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			tknClaims, err := GetRawTokenClaims(adaptr.GetCtxValue(r, ctxTokenKey).(string))
+			if err != nil {
+				http.Error(w, "can not get tkn claims err="+err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			userIdent := tknClaims.Subject
+			if userIdent == "" {
+				http.Error(w, "empty user ident in token", http.StatusBadRequest)
+				return
+			}
+			_, _, err = apiKey.ParseUserIdent(userIdent)
+			if err != nil {
+				http.Error(w, "user ident err="+err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			audience := tknClaims.Audience
+			if audience == "" {
+				http.Error(w, "empty audience in token", http.StatusBadRequest)
+				return
+			}
+
+			_, err = apiKey.ParseWorkspaceIdentString(audience)
+			if err != nil {
+				http.Error(w, "audience parse err="+err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			h.ServeHTTP(w, adaptr.SetCtxValue(adaptr.SetCtxValue(r, ctxUserIdentKey, userIdent), tknAudKey, audience))
+		})
+	}
+}
+
+func getTokenPayloadPartRaw(rawJWToken string) (string, error) {
+	if len(rawJWToken) < 5 {
+		return "", errors.New("token length too small")
+	}
+	if strings.Count(rawJWToken, ".") != 2 {
+		return "", ErrNotAToken
+	}
+	rawSplit := strings.Split(rawJWToken, ".")
+	return rawSplit[1], nil
+}
+
+func GetRawTokenClaims(rawJWToken string) (*jwt.StandardClaims, error) {
+	var claims *jwt.StandardClaims = nil
+	payloadRaw, err := getTokenPayloadPartRaw(rawJWToken)
+	if err != nil {
+		return claims, err
+	}
+
+	payloadStr, err := jwt.DecodeSegment(payloadRaw)
+	if err != nil {
+		return claims, fmt.Errorf("err decoding tkn payload err=%v", err)
+	}
+	claims = &jwt.StandardClaims{}
+	err = json.Unmarshal([]byte(payloadStr), claims)
+	if err != nil {
+		return claims, fmt.Errorf("GetRawTokenClaims unmarshall err=%v \n payload=%v", err, string(payloadStr))
+	}
+	return claims, nil
 }
